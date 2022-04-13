2ff55ae1b5cf0150d0c44b6d9a887f30a36fd199
Gregor Gabriel <gregor.gabriel@unit.no>
2022-04-13 10:42:52 +0200

    SMILE-1529: pmd 82

--- src/main/java/no/sikt/oai/ValidParameterKey.java
+++ src/main/java/no/sikt/oai/ValidParameterKey.java
@@ -1,7 +1,7 @@
 package no.sikt.oai;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public enum ValidParameterKey {
     
@@ -12,8 +12,10 @@ public enum ValidParameterKey {
         UNTIL("until"),
         SET("set"),
         RESUMPTIONTOKEN("resumptionToken");
-        String key = "";
-        public static final Map<String, ValidParameterKey> keyMap = new HashMap<String, ValidParameterKey>();
+
+        String key;
+
+        public static final Map<String, ValidParameterKey> keyMap = new ConcurrentHashMap<>();
 
         static {
             for (ValidParameterKey parameterKey : values()) {
